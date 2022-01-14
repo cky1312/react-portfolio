959b81095b3f280114d8a885adbf26f1141250bd
chandreshYadav <chandresh.yadav@leadsquared.com>
2022-01-14 10:21:21 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import Navbar from "./components/Navbar"
+import Navbars from "./components/Navbar";
 
-export default class App extends React.Component{
-
-
-  render(){
-    return(
-     <div> 
-      <Navbar/>
-      <h1>Hello React</h1>
-     </div>
-    )
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbars />
+        <h1>Hello React</h1>
+      </div>
+    );
   }
 }
